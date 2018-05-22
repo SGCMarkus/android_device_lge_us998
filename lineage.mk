a6a46e81a06ce2d6b3e6be3cd375f7ae058662fe
SGCMarkus <markusornik@gmail.com>
2018-05-22 15:39:13 +0200

    us998: initial commit, converted from H930

---
+++ lineage.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/lge/us998/full_us998.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_NAME := lineage_us998
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="joan" \
+    PRODUCT_DEVICE="joan" \
+    PRODUCT_NAME="joan_global_com" \
+    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 180851011dea3 release-keys"
+
+BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/180851011dea3:user/release-keys"
